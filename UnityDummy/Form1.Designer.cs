7f999e858b127759841d0ba284193f1bf42d7e6e
rhupy1 <fmsj@naver.com>
2022-06-12 15:36:06 +0900

    초기 커밋.

---
+++ UnityDummy/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace UnityDummy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(268, 313);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 342);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "-> 보내기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Unity";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button button1;
+    }
+}
---
+++ UnityDummy/Form1.cs
@@ -0,0 +1,104 @@
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+
+namespace UnityDummy
+{
+    public partial class Form1 : Form
+    {
+        //파이프
+        NamedPipeClientStream PipeClient = new NamedPipeClientStream(".", "UI to Unity", PipeDirection.Out, PipeOptions.None, TokenImpersonationLevel.Impersonation);
+        NamedPipeServerStream PipeServer = new NamedPipeServerStream("Unity to UI", PipeDirection.In);
+
+        String ReturnString
+        {
+            set
+            {
+                this.Invoke(new EventHandler(delegate {
+                    richTextBox1.AppendText(value + "\r\n");
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToCaret();
+                }));
+            }
+        }
+        String InputString
+        {
+            get
+            {
+                string r = string.Empty;
+                this.Invoke(new EventHandler(
+                        delegate
+                        {
+                            r = richTextBox2.Text;
+                        }));
+                return r;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            //메세지 받는 파이프 준비
+            Task.Factory.StartNew(() => PipeRecieve());
+        }
+        public void PipeRecieve()
+        {
+            PipeServer.WaitForConnection();
+            ReturnString = "PipeServer 연결 생성";
+            while (PipeServer.IsConnected)
+            {
+                var read = new byte[4096];
+                int iRet = PipeServer.Read(read, 0, read.Length);
+                if (iRet > 0)
+                {
+                    var msg = Encoding.UTF8.GetString(read);
+                    ReturnString = "[Recieve]" + msg;
+                }
+            }
+        }
+        public void PipeSend()
+        {
+            string msg = InputString;
+            while (true)
+            {
+                if (!PipeClient.IsConnected)
+                {
+                    try
+                    {
+                        PipeClient.Connect();
+                        Task.Delay(500);
+                        ReturnString = "PipeClient 연결 생성";
+                    }
+                    catch (Exception ee)
+                    {
+                        ReturnString = ee.Message;
+                    }
+                    continue;
+                }
+
+                try
+                {
+                    StreamWriter writer = new StreamWriter(PipeClient);
+                    writer.WriteLine(msg);
+                   
+                    this.Invoke(new EventHandler(
+                        delegate
+                        {
+                            ReturnString = "[Send]" + msg;
+                        }));
+                    writer.Flush();
+                }
+                catch (Exception ee)
+                {
+                    ReturnString = ee.Message;
+                }
+                break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() => PipeSend());
+        }
+    }
+}
